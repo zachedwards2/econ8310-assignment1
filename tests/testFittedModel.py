8315db93b8750abe0195d9a9339b866fb7156ded
Dusty White <dusty.white@gmail.com>
2025-01-08 12:23:55 -0600

    replace arima, ols with var, varmax models

--- tests/testFittedModel.py
+++ tests/testFittedModel.py
@@ -8,8 +8,8 @@ import pygam
 from statsmodels.tsa.holtwinters import ExponentialSmoothing as ES
 import prophet
 import statsmodels.tsa.holtwinters as hw
-from statsmodels.regression.linear_model import RegressionResultsWrapper
-from statsmodels.tsa.arima.model import ARIMAResultsWrapper
+from statsmodels.tsa.statespace.varmax import VARMAXResultsWrapper
+from statsmodels.tsa.vector_ar.var_model import VARResultsWrapper
 
 # Import your code from parent directory
 import os
@@ -26,7 +26,7 @@ from assignment1 import model, modelFit, pred
 
 class testCases(unittest.TestCase):
     def testFittedModel(self):
-        if isinstance(modelFit, RegressionResultsWrapper) or isinstance(modelFit, ARIMAResultsWrapper) or isinstance(modelFit, hw.results.HoltWintersResultsWrapper):
+        if isinstance(modelFit, VARResultsWrapper) or isinstance(modelFit, VARMAXResultsWrapper) or isinstance(modelFit, hw.results.HoltWintersResultsWrapper):
             self.assertTrue(True)
         elif hasattr(modelFit, '_is_fitted'):
             if modelFit._is_fitted:
