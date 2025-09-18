3155ae170893ec73f1a5a96e2d07857951dc1e5b
zachedwards2 <Zacharyedwards@unomaha.edu>
2025-09-17 19:20:57 -0500

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -21,5 +21,18 @@ y_train = train_2018['trips'].values
 model = LinearGAM(
     s(0, n_splines=24, spline_order=3) +      # hour of day
     s(1, n_splines=7, spline_order=3) +       # day of week
-    s(2, n_splines=30, spline_order=3)        # day of year (trend)
+    s(2, n_splines=30, spline_order=3)        # day of year
 )
+
+modelFit = model.fit(X_train, y_train)
+
+pred_dates = pd.date_range(start='2019-01-01 00:00:00', end='2019-01-31 23:00:00', freq='H')
+pred_df = pd.DataFrame()
+pred_df['datetime'] = pred_dates
+pred_df['hour'] = pred_df['datetime'].dt.hour
+pred_df['day_of_week'] = pred_df['datetime'].dt.dayofweek
+pred_df['day_of_year'] = pred_df['datetime'].dt.dayofyear
+
+X_pred = pred_df[['hour','day_of_week','day_of_year']].values
+pred = modelFit.predict(X_pred)
+
